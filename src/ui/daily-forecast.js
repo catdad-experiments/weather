6a370e3b84432726f57f3c5f3cce106171a80865
Kiril Vatev <vatev.1@gmail.com>
2023-01-07 23:32:46 -0500

    cleaning up rendering

--- src/ui/daily-forecast.js
+++ src/ui/daily-forecast.js
@@ -1,7 +1,24 @@
 import { html } from '../preact.js';
 import { useWeather } from '../weather.js';
-
 import { getDayName, getTime } from '../utils.js';
+
+const Day = ({ data }) => {
+  const {
+    date,
+    temperatureMinStr, temperatureMaxStr,
+    feelsLikeMinStr, feelsLikeMaxStr,
+    precipitation, precipitationStr,
+    weatherStr,
+    sunrise, sunset
+  } = data;
+
+  return html`<div style="border: 1px solid gray; padding: 10px; margin: 10px; border-radius: 10px" >
+    <div>${getDayName(date, 'long')} - ${weatherStr}<//>
+    <div>${temperatureMinStr} (${feelsLikeMinStr}) / ${temperatureMaxStr} (${feelsLikeMaxStr})<//>
+    ${precipitation > 0 ? html`<div>🌧 ${precipitationStr}<//>` : ''}
+    <div>☀⬆: ${getTime(sunrise)}, ☀⬇: ${getTime(sunset)}<//>
+  <//>`;
+};
 
 export const DailyForecast = () => {
   const { weather } = useWeather();
@@ -12,25 +29,5 @@ export const DailyForecast = () => {
 
   const { daily } = weather.value;
 
-  const elems = [];
-
-  for (const data of daily) {
-    const {
-      date,
-      temperatureMinStr, temperatureMaxStr,
-      feelsLikeMinStr, feelsLikeMaxStr,
-      precipitation, precipitationStr,
-      weatherStr,
-      sunrise, sunset
-    } = data;
-
-    elems.push(html`<div style="border: 1px solid gray; padding: 10px; margin: 10px; border-radius: 10px" >
-      <div>${getDayName(date, 'long')} - ${weatherStr}<//>
-      <div>${temperatureMinStr} (${feelsLikeMinStr}) / ${temperatureMaxStr} (${feelsLikeMaxStr})<//>
-      ${precipitation > 0 ? html`<div>🌧 ${precipitationStr}<//>` : ''}
-      <div>☀⬆: ${getTime(sunrise)}, ☀⬇: ${getTime(sunset)}<//>
-    <//>`);
-  }
-
-  return html`<div>${elems}<//>`;
+  return html`<div>${daily.map(data => html`<${Day} data=${data} />`)}<//>`;
 };
--- src/ui/hourly-forecast.js
+++ src/ui/hourly-forecast.js
@@ -2,6 +2,27 @@ import { html } from '../preact.js';
 import { useWeather } from '../weather.js';
 import { getDayName, getHour } from '../utils.js';
 
+const Hour = ({ data }) => {
+  const {
+    date,
+    temperatureStr, feelsLikeStr,
+    precipitation, precipitationStr,
+    rain, rainStr,
+    snowfall, snowfallStr,
+    showers, showersStr,
+    weatherStr
+  } = data;
+
+  return html`<div style="border: 1px solid gray; padding: 10px; margin: 10px; border-radius: 10px" >
+    <div>${getDayName(date)} ${getHour(date)}, ${weatherStr}<//>
+    <div><b>${temperatureStr}</b>, feels like ${feelsLikeStr}<//>
+    ${precipitation > 0 ? html`<div>🌧 ${precipitationStr}<//>` : ''}
+    ${rain > 0 ? html`<div>  💧 ${rainStr}<//>` : ''}
+    ${snowfall > 0 ? html`<div>  ❄ ${snowfallStr}<//>` : ''}
+    ${showers > 0 ? html`<div>  🚿 ${showersStr}<//>` : ''}
+  <//>`;
+};
+
 export const HourlyForecast = () => {
   const { weather } = useWeather();
 
@@ -11,33 +32,12 @@ export const HourlyForecast = () => {
 
   const { hourly } = weather.value;
 
-  const elems = [];
-
-  for (const data of hourly) {
-    const {
-      date,
-      temperatureStr, feelsLikeStr,
-      precipitation, precipitationStr,
-      rain, rainStr,
-      snowfall, snowfallStr,
-      showers, showersStr,
-      weatherStr
-    } = data;
-
-    // don't show time that has already passed
-    if (date < new Date()) {
-      continue;
-    }
-
-    elems.push(html`<div style="border: 1px solid gray; padding: 10px; margin: 10px; border-radius: 10px" >
-      <div>${getDayName(date)} ${getHour(date)}, ${weatherStr}<//>
-      <div><b>${temperatureStr}</b>, feels like ${feelsLikeStr}<//>
-      ${precipitation > 0 ? html`<div>🌧 ${precipitationStr}<//>` : ''}
-      ${rain > 0 ? html`<div>  💧 ${rainStr}<//>` : ''}
-      ${snowfall > 0 ? html`<div>  ❄ ${snowfallStr}<//>` : ''}
-      ${showers > 0 ? html`<div>  🚿 ${showersStr}<//>` : ''}
-    <//>`);
-  }
-
-  return html`<div>${elems}<//>`;
+  return html`
+    <div>
+      ${hourly
+        // don't show hours earlier than right now
+        .filter(({ date }) => date > new Date())
+        .map(data => html`<${Hour} data=${data} />`)
+      }
+    <//>`;
 };
