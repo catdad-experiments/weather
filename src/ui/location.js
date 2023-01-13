ab781d1f64f4dd4be00082bc50b757f25f0e5d76
Kiril Vatev <vatev.1@gmail.com>
2023-01-12 21:39:51 -0500

    cleaning up location display

--- src/ui/location.js
+++ src/ui/location.js
@@ -1,22 +1,45 @@
 import { html } from '../preact.js';
 import { useWeather } from '../weather.js';
+import { useStyle } from '../style.js';
 
 export const Location = () => {
   const { location, weather } = useWeather();
+
+  const classname = useStyle(`
+    $ {
+      text-align: center;
+      margin: 1rem 0;
+    }
+
+    $ h1 {
+      padding: 0;
+      margin: 0.5rem 0;
+      font-size: 2rem;
+    }
+
+    $ h2 {
+      padding: 0;
+      margin: 0.2rem 0;
+      font-size: 1.5rem;
+    }
+  `);
 
   if (!location.value) {
     return;
   }
 
-  const { latitude, longitude, informative, administrative } = location.value;
+  const { latitude, longitude, informative, administrative, city } = location.value;
   const { elevation } = weather.value || {};
 
-  return html`
+  return html`<div class="${classname}">
     ${administrative ?
-      html`<h2>${administrative} (${informative})<//>` :
-      html`<h2>Unknown location<//>`
+      html`
+        <h1>${administrative}<//>
+        <h2>${city}, ${informative}<//>
+      ` :
+      html`<h1>Unknown location<//>`
     }
-    <sub>${latitude}, ${longitude}<//>
-    <p>Elevation: ${elevation}<//>
-  `;
+    <div>${latitude}, ${longitude}<//>
+    <div>Elevation: ${elevation}<//>
+  <//>`;
 };
