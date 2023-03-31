bf692fe4473450e6329e11f8a35ced69a3fb97f0
Kiril Vatev <vatev.1@gmail.com>
2023-03-31 11:52:08 -0400

    more ui cleanup

--- src/ui/daily-forecast.js
+++ src/ui/daily-forecast.js
@@ -25,7 +25,7 @@ const Day = ({ data }) => {
       padding: 10px;
       margin: 10px;
       border-radius: 10px;
-      line-height: 1.2;
+      line-height: 1.4;
     }
 
     $ .toggle {
@@ -44,10 +44,12 @@ const Day = ({ data }) => {
     <div class="toggle" onclick=${() => { hourlyVisible.value = !hourlyVisible.value; }}><${Emoji}>${hourlyVisible.value ? '🔼' : '🔽'}<//><//>
 
     <div>${getDayName(date, 'long')}, ${getDate(date)} - <${Emoji}>${weatherIcon}<//> ${weatherStr}<//>
-    <div>↟${temperatureMaxStr} • ↡${temperatureMinStr}<//>
-    <div class="dim">feels like ↟${feelsLikeMaxStr} • ↡${feelsLikeMinStr}<//>
+    <div>
+      <span title="high">${temperatureMaxStr}<//> ↿⇂ <span title="low">${temperatureMinStr} <//>
+      <span class="dim" title="feels like">(${feelsLikeMaxStr} ↿⇂ ${feelsLikeMinStr})<//>
+    <//>
     ${precipitation > 0 ? html`<div>🌧 ${precipitationStr}<//>` : ''}
-    <div><${Emoji}>🌅<//>: ${getTime(sunrise)}, <${Emoji}>🌇<//>: ${getTime(sunset)}<//>
+    <div><${Emoji}>🌅<//>: ${getTime(sunrise)} • <${Emoji}>🌇<//>: ${getTime(sunset)}<//>
 
     ${hourlyVisible.value ? html`<${HourlyForecast} hourly=${hourly} />` : ''}
   <//>`;
