1e7c9b51696f16b89a5a03053e371079d375a1ab
Kiril Vatev <vatev.1@gmail.com>
2023-12-21 00:41:31 -0500

    adding a scripted build for the icons

---
+++ assets/build-icons.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { promises: fs } = require('fs');
+const { generateIcons } = require('svg-app-icon');
+
+const input = (...parts) => path.resolve(__dirname, ...parts);
+const output = (...parts) => path.resolve(__dirname, '../icons', ...parts);
+
+(async () => {
+  await fs.rm(output(), { force: true, recursive: true });
+  await fs.mkdir(output());
+
+  const svg = await fs.readFile(input('icon.svg'));
+
+  for await (const icon of generateIcons(svg, {
+    ico: false,
+    icns: false,
+    svg: true,
+    png: true,
+    pngSizes: [48, 192, 512]
+  })) {
+    await fs.writeFile(output(icon.name), icon.buffer);
+  }
+})();
