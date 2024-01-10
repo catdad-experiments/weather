bc11d3cc36410775a8fe0cb770fd9fcf789a8456
Kiril Vatev <vatev.1@gmail.com>
2024-01-09 20:54:48 -0500

    removing unnecessary separate file for backgrounds, adding a background to maskable image
    maskable images are filled with white in transparent areas, so a solid color is needed to display a color you control

--- assets/build-icons.js
+++ assets/build-icons.js
@@ -5,14 +5,15 @@ const { generateIcons } = require('svg-app-icon');
 const input = (...parts) => path.resolve(__dirname, ...parts);
 const output = (...parts) => path.resolve(__dirname, '../icons', ...parts);
 
+const background = '#cac9c8';
+
 (async () => {
   await fs.rm(output(), { force: true, recursive: true });
   await fs.mkdir(output());
 
   const svg = await fs.readFile(input('icon.svg'));
-  const mask = await fs.readFile(input('icon-mask.svg'));
 
-  for await (const icon of generateIcons([mask, svg], {
+  for await (const icon of generateIcons([`<svg viewBox="0 0 10 10"><circle cx="5" cy="5" r="5" fill="${background}"/></svg>`, svg], {
     ico: false,
     icns: false,
     svg: true,
@@ -22,12 +23,12 @@ const output = (...parts) => path.resolve(__dirname, '../icons', ...parts);
     await fs.writeFile(output(icon.name), icon.buffer);
   }
 
-  for await (const icon of generateIcons([svg], {
+  for await (const icon of generateIcons([`<svg viewBox="0 0 10 10"><rect x="0" y="0" width="10" height="10" fill="${background}" /></svg>`, svg], {
     ico: false,
     icns: false,
     svg: false,
     png: true,
-    pngSizes: [512]
+    pngSizes: [48, 192, 512]
   })) {
     const name = `${path.parse(icon.name).name}-maskable.${icon.ext}`;
     await fs.writeFile(output(name), icon.buffer);
