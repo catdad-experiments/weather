1f1bed9fa581f56ce2bd969f029fbc674cfe0267
Kiril Vatev <vatev.1@gmail.com>
2024-01-22 01:18:23 -0500

    if we don't have permission to use device position, navigate to manual location selection

--- src/hooks/location.js
+++ src/hooks/location.js
@@ -1,25 +1,31 @@
 import { createContext, useContext, useEffect, html, useSignal } from '../preact.js';
 import { getPosition } from '../sources/position.js';
+import { useRoutes } from './routes.js';
 
 const Location = createContext({});
 
 export const withLocation = Component => ({ children, ...props }) => {
   const location = useSignal(null);
+  const { route, ROUTES } = useRoutes();
 
   useEffect(() => {
     (async () => {
-      // const { state: geolocationPermission } = await navigator.permissions.query({ name: 'geolocation' });
+      const { state: geolocationPermission } = await navigator.permissions.query({ name: 'geolocation' });
 
-      const position = await getPosition();
-      console.log('got new location', position);
-      location.value = { ...position, type: 'device' };
+      if (geolocationPermission === 'granted') {
+        const position = await getPosition();
+        console.log('got new location', position);
+        location.value = { ...position, type: 'device' };
+      } else {
+        route.value = ROUTES.location;
+      }
     })().catch(err => {
       // TODO
       console.error('failed to get position:', err);
     });
   }, []);
 
-  if (!location.value) {
+  if (!location.value && route.value !== ROUTES.location) {
     return html`<div>Getting location...<//>`;
   }
 
